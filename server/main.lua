d47df994f655fd509f606418ffa687be03d3f055
JackDUpModZ <83776161+JackDUpModZ@users.noreply.github.com>
2023-03-01 08:09:41 +0000

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -5,7 +5,7 @@
 -- 	print(k,v)
 -- end
 
-RegisterNetEvent("JD_CommunityService:sendToService", function(target, actions)
+sendToService = function(target, actions)
 	local senderPlayer = ESX.GetPlayerFromId(source)
 	local targetPlayer = ESX.GetPlayerFromId(target)
 
@@ -21,7 +21,7 @@ RegisterNetEvent("JD_CommunityService:sendToService", function(target, actions)
 	else
 		senderPlayer.showNotification('Invalid ID / No one sent!')
 	end
-end)
+end
 
 RegisterNetEvent("JD_CommunityService:updateService", function(target, actions)
 	local _source = target -- cannot parse source to client trigger for some weird reason
@@ -42,8 +42,6 @@ AddEventHandler('JD_CommunityService:completeService', function()
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	local identifier = xPlayer.identifier
 	local delete = MySQL.query.await('DELETE FROM communityservice WHERE identifier = ?', {identifier})
-
-	--TriggerClientEvent('JD_CommunityService:finishCommunityService', target)
 end)
 
 lib.callback.register('JD_CommunityService:getCurrentActions', function()
@@ -52,4 +50,37 @@ lib.callback.register('JD_CommunityService:getCurrentActions', function()
 	local identifier = xPlayer.identifier
 	local count = MySQL.scalar.await('SELECT actions_remaining FROM communityservice WHERE identifier = ?', {identifier})
 	return count
+end)
+
+RegisterCommand('communityService', function(source, args, rawCommand)
+	local _source = source -- cannot parse source to client trigger for some weird reason
+	local xPlayer = ESX.GetPlayerFromId(_source)
+
+	if xPlayer.job.name == Config.PoliceJob then
+		local input = lib.inputDialog('Community Service', {'Player ID', 'Number of actions'})
+
+		if not input then return end
+		local targetID = tonumber(input[1])
+		local actionCount = input[2]
+		sendToService(targetID, actionCount)
+	else
+		xPlayer.showNotification('No permissions to access this!')
+	end
+end,false)
+
+RegisterServerEvent('JD_CommunityService:communityMenu')
+AddEventHandler('JD_CommunityService:communityMenu', function()
+	local _source = source -- cannot parse source to client trigger for some weird reason
+	local xPlayer = ESX.GetPlayerFromId(_source)
+
+	if xPlayer.job.name == Config.PoliceJob then
+		local input = lib.inputDialog('Community Service', {'Player ID', 'Number of actions'})
+
+		if not input then return end
+		local targetID = tonumber(input[1])
+		local actionCount = input[2]
+		sendToService(targetID, actionCount)
+	else
+		xPlayer.showNotification('No permissions to access this!')
+	end
 end)
